2178006895f9ee45c2147560360dd01e55b9e92d
alexisjcarr <acarr59@gmail.com>
2019-07-20 13:05:17 -0500

    🔥 Adds 2 solutions to anagrams.
     #1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,68 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const anagrams = (stringA, stringB) => {
+  return (
+    stringA
+      .replace(/[^\w]/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("") ===
+    stringB
+      .replace(/[^\w]/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("")
+  );
+};
 
 module.exports = anagrams;
+
+/* NOTES */
+
+// word.replace(/[^\w]/g, "");
+/*
+const obj = {
+    a: 1,
+    b: 1,
+    c: 1
+};
+
+Object.keys(obj).length;
+*/
+
+/* 
+const numbers = [10, 30, 5, -90, 10000];
+numbers = ["z", "c", "b", "d", "y"]
+numbers.sort()
+*/
+
+/* SOLUTIONS */
+
+// const anagrams = (stringA, stringB) => {
+//     const charMapA = buildCharMap(stringA);
+//     const charMapB = buildCharMap(stringB);
+
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//       return false;
+//     }
+
+//     for (let char in charMapA) {
+//       if (charMapA[char] !== charMapB[char]) {
+//         return false;
+//       }
+//     }
+
+//     return true;
+//   };
+
+//   function buildCharMap(str) {
+//     const charMap = {};
+
+//     for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//       charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+//   }
