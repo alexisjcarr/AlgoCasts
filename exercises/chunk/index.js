5749a8f3b036b92e964cabb2646c88d89961740d
alexisjcarr <acarr59@gmail.com>
2019-07-19 23:47:12 -0500

    ✅ Adds two solutions to chunk
     #1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (array, size) => {
+  const newArr = [];
+  let i = 0;
+  while (i < array.length) {
+    newArr.push(array.slice(i, i + size));
+    i += size;
+  }
+  return newArr;
+};
+
+console.log(chunk([1, 2, 3, 4], 2));
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(chunk([1, 2, 3, 4, 5], 4));
+console.log(chunk(chunk([1, 2, 3, 4, 5], 10)));
 
 module.exports = chunk;
+
+// const chunk = (array, size) => {
+//   const newArr = [];
+
+//   for (let el of array) {
+//     const last = newArr[newArr.length - 1];
+
+//     if (!last || last.length === size) {
+//       newArr.push(el);
+//     } else {
+//       last.push(el);
+//     }
+//   }
+//   return newArr;
+// };
